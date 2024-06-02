86087a88b2aca9dd60a213d83ad5646214c18681
JohelT <66223732+JohelT@users.noreply.github.com>
2024-06-01 18:19:20 -0700

    messing with padding

--- src/App.svelte
+++ src/App.svelte
@@ -27,7 +27,7 @@
   };
 </script>
 
-<section><b>Introduction<b><br>Climate change denial is still a rampant issue. Climate change, while heavily displayed in media, never seems to have easily accessible scientific data. As the timescale is larger than humans were made for, climate change has a lot of plausible deniability. This project aims to reduce that issue.</section>
+<section><br><br><br><br><br><br><b>Introduction</b><br>Climate change denial is still a rampant issue. Climate change, while heavily displayed in media, never seems to have easily accessible scientific data. As the timescale is larger than humans were made for, climate change has a lot of plausible deniability. This project aims to reduce that issue.</section>
 
 <Scroller
   top={0.0}
@@ -41,9 +41,9 @@
 
 <div class="foreground" slot="foreground">
   
-  <section><b>Pre-industrialization<b><br>Pre-industrialization, climate change was a natural phenomina, yet occured gradually. On the right is the data of climate change before large scale human interaction. </section>
-  <section><b>Current Day<b><br>Vizualised here is how we have treated the planet over the past ~100 years. We have seen historic and irreversable increases globally.</section>
-  <section><b>Local Data<b><br>Here is the data broken down locally. Feel free to find your city to learn more about how climate change affects your local community.</section>
+  <section><b>Pre-industrialization</b><br>Pre-industrialization, climate change was a natural phenomina, yet occured gradually. On the right is the data of climate change before large scale human interaction. </section>
+  <section><b>Current Day</b><br>Vizualised here is how we have treated the planet over the past ~100 years. We have seen historic and irreversable increases globally.</section>
+  <section><b>Local Data</b><br>Here is the data broken down locally. Feel free to find your city to learn more about how climate change affects your local community.</section>
   
 </div>
 
@@ -57,7 +57,7 @@
 </div>
 </Scroller>
 
-<section><b>Call to Action<b><br>Action must be taken to prevent climate change before it is too late. Attached is a variety of sources from groups much more informed than we are, sorted into categories depending on what you are looking for.<br>
+<section><b>Call to Action</b><br>Action must be taken to prevent climate change before it is too late. Attached is a variety of sources from groups much more informed than we are, sorted into categories depending on what you are looking for.<br>
 <br>
 Individual impact:<br>
 <a href="https://www.un.org/en/actnow/ten-actions">www.un.org/</a><br>
@@ -85,7 +85,7 @@ The Effects of Climate Change:<br>
   }
 
   .foreground {
-    padding: 150px 0;
+    padding: 125x 0;
     margin-left: 0;
     margin-right: auto;
     height: auto;
